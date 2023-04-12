195e5551a5a56c1ca8d448d73a1645ed9b31ab0e
권 도윤 <doyoun8813@i-commtech.com>
2023-04-12 23:12:51 +0900

    게이지 단순하게 등록하는 버전

--- src/main/java/hello/ActuatorApplication.java
+++ src/main/java/hello/ActuatorApplication.java
@@ -10,6 +10,7 @@ import hello.order.V2.OrderConfigV2;
 import hello.order.V3.OrderConfigV3;
 import hello.order.V4.OrderConfigV4;
 import hello.order.gauge.StockConfigV1;
+import hello.order.gauge.StockConfigV2;
 import hello.order.v0.OrderConfigV0;
 import hello.order.v0.OrderServiceV0;
 import hello.order.v1.OrderConfigV1;
@@ -19,7 +20,8 @@ import hello.order.v1.OrderConfigV1;
 // @Import(OrderConfigV2.class)
 // @Import(OrderConfigV3.class)
 // @Import(OrderConfigV4.class)
-@Import({OrderConfigV4.class, StockConfigV1.class})
+// @Import({OrderConfigV4.class, StockConfigV1.class})
+@Import({OrderConfigV4.class, StockConfigV2.class})
 @SpringBootApplication(scanBasePackages = "hello.controller")
 public class ActuatorApplication {
 
